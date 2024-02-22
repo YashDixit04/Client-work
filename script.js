aec7b44c8a51f50ef8d43068c0399da3642f5158
Yash dixit <yash9369d@gmail.com>
2024-02-22 13:46:33 +0530

    Update-headings

--- script.js
+++ script.js
@@ -25,10 +25,6 @@ window.addEventListener("load", function(){
 })
 
 AOS.init();
-//script for scroll to top
-$("#top-button").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, 1000);
-});
 
 //script for light box
 $(document).on('click', '[data-toggle="lightbox"]', function (event) {
